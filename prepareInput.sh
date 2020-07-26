2ac2052147d74cef24d35742fe2b4e5f506a7ebb
strah <strah@donttell>
2020-07-26 11:45:47 +0200

    Script updated

--- prepareInput.sh
+++ prepareInput.sh
@@ -3,11 +3,13 @@
 INPUT=$1
 OUTPUT=data/input
 
+getGenre() { ffprobe "$1" 2>&1 | sed -E -n 's/^ *genre *: (.*)/\1/p'; }
+
 OIFS="$IFS"
 IFS=$'\n'
 for i in $(find ${INPUT} -depth -iname "*.mp3")
 do
-  GENRE=$(echo "$i" | perl -pe 's/^.+\(.+\)\((.+)\)(.+)$/$1/')
+  GENRE="$(getGenre $i | tr ' ' '_')"
   DST="$OUTPUT/$GENRE/$(uuidgen).mp3"
 
   mkdir -p "$OUTPUT/$GENRE"
--- train.cpp
+++ train.cpp
@@ -7,9 +7,16 @@ Train features extractor
 #include <essentia/algorithmfactory.h> 
 #include <essentia/utils/extractor_music/extractor_version.h>
 #include <essentia/pool.h>
+#include <filesystem>
+namespace fs = std::filesystem;
 
 using namespace essentia::standard;
 
 int main(int argc, char* argv[]) {
     essentia::init();
+
+    std::string input = "data/input";
+    for (const auto & genreDir : fs::directory_iterator(input)) {
+        std::cout << genreDir.path() << std::endl;
+    }
 }
